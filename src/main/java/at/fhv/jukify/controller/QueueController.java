10f8f69c5b3e25409f86c565d3ce901c7f2d025e
moritz-w <mwi4339@students.fhv.at>
2018-05-05 10:02:04 +0200

    Queue Controller extended with local queue

--- src/main/java/at/fhv/jukify/controller/QueueController.java
+++ src/main/java/at/fhv/jukify/controller/QueueController.java
@@ -8,14 +8,16 @@ import at.fhv.jukify.spotify_container.exception.SpotifyAuthException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class QueueController {
 
     private static LinkedList<TrackPojo> queue = new LinkedList<>();
     private static PlaylistPojo playlistQueue;
     private static PlaylistPojo sourcePlaylist;
+
+    private static Iterator<TrackPojo> sourcePlayListTrackPaging;
 
     public List<PlaylistPojo> getUserPlaylists(){
         try {
@@ -35,17 +37,52 @@ public class QueueController {
         SpotifyPlaylist playlist = SpotifyPlaylist.getInstance();
         try {
             playlistQueue = playlist.createPlaylist(playlistNameString);
+            sourcePlayListTrackPaging = playlist.getTracksFromPlaylist(sourcePlaylist.getPlaylistID()).iterator();
         } catch (SpotifyAuthException e) {
             e.printStackTrace();
         }
     }
 
     public void addOrUpdateVote(String trackID){
+        boolean found = false;
+        for (TrackPojo trackPojo : queue) {
+            if(trackPojo.getTrackID().equals(trackID)){
+                found = true;
+                trackPojo.setVoteCount(trackPojo.getVoteCount() + 1);
+            }
+        }
+        if(!found){
+            TrackPojo tempPojo = new TrackPojo();
+            tempPojo.setVoteCount(1);
+            tempPojo.setTrackID(trackID);
+            queue.add(tempPojo);
+        }
+    }
 
+    public String getNextTrackFromQueue(){
+        Collections.sort(queue, new Comparator<TrackPojo>() {
+            @Override
+            public int compare(TrackPojo o1, TrackPojo o2) {
+                return Integer.compare(o1.getVoteCount(), o2.getVoteCount());
+            }
+        });
+        return queue.getFirst().getTrackID();
     }
 
     public void addFromSourcePlaylist(){
-
+        SpotifyPlaylist playlist = SpotifyPlaylist.getInstance();
+        if(sourcePlayListTrackPaging.hasNext()){
+            try {
+                playlist.addToPlaylist(playlistQueue.getPlaylistID(), sourcePlayListTrackPaging.next().getTrackID());
+            } catch (SpotifyAuthException e) {
+                e.printStackTrace();
+            }
+        } else {
+            List<TrackPojo> trackPojos = playlist.crawlNextPage();
+            if(!trackPojos.isEmpty()){
+                sourcePlayListTrackPaging = trackPojos.iterator();
+            }
+        }
     }
 
 }
